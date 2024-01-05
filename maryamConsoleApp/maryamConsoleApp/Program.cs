43e940b8e72395fb5d4a40bda19bf29bf680928e
Wobbey <dominic.prevost04@gmail.com>
2024-01-05 15:13:31 -0500

    push with .gitignore

---
+++ maryamConsoleApp/maryamConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿
+public delegate void MonDelegateFunctionClass(string textSample);
+public class program
+{
+    static void Main(string[] args)
+    {
+        MonDelegateFunctionClass monDelegateFunctionObject = ToUpperCase;
+
+        EcritPourMoiFunction("pas d'objet");
+        EcritPourMoiFunction("aLlloOooO!!", monDelegateFunctionObject);
+
+
+        monDelegateFunctionObject = ToLowerCase;
+
+        EcritPourMoiFunction("aLlloOooO!!", monDelegateFunctionObject);
+        Console.ReadKey();
+    }
+
+
+    static void ToUpperCase(string Text) => Console.WriteLine(Text.ToUpper());
+    static void ToLowerCase(string Text) => Console.WriteLine(Text.ToLower());
+
+
+
+    static void EcritPourMoiFunction(string unText, MonDelegateFunctionClass inconnu = null)
+    {
+        Console.WriteLine("$Before:{unText}");
+        if (inconnu != null)
+        {
+            inconnu(unText);
+        }
+
+    }
+
+
+
+
+}
